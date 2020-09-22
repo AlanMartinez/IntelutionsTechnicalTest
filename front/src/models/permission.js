bf8a02f407e7ca6c383ed8bc2c77b6a30be12efc
Alan <alann.m-@hotmail.com>
2020-09-22 15:24:57 -0300

    Delete permission.js

--- front/src/models/permission.js
+++
@@ -1,8 +0,0 @@
-function Permission (name, lastname, permissionType, date) {
-  this.Nombre = name
-  this.Apellido = lastname
-  this.TipoPermiso = permissionType 
-  this.Fecha = date
-}
-
-export {Permission}
